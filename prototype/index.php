76872d684bdd9bd1ad71d2757d98452fec0d29c6
elyatim-omar <elyatim.omar.dev@gmail.com>
2021-04-19 09:31:48 +0000

    prototype

--- prototype/index.php
+++ prototype/index.php
@@ -1,14 +1,14 @@
 
       <form action = "index.php" method = "post">
-          <input type = "text" name = "num1" >
-          <input type = "text" name = "num2" >
+          <input type = "number" name = "num1" >
+          <input type = "number" name = "num2" >
          <input type = "submit" >
       </form>
 
   
 
 <?php
-
+ 
 if(isset($_POST['num1'],$_POST['num2'])){
     $num1 = $_POST['num1'];
     $num2 = $_POST['num2'];
